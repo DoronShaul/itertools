28c53c2016cd52ace94a65a7e04cf0d505b1cb59
Doron <doronsds@gmail.com>
2019-05-15 00:19:17 +0300

    implements product and powerset

--- powerset.hpp
+++ powerset.hpp
@@ -1,37 +1,105 @@
 #pragma once
 #include <iostream>
-#include <iterator>
 using namespace std;
 
 namespace itertools
 {
-template <class T>
-class powerset
+template <typename T>
+class powersetIterator
 {
 private:
-    T first;
+    const T powerSet;
+
+public:
+    powersetIterator(const T &a) : powerSet(a) {}
 
     class iterator
     {
-    private:
-        T one;
+        pair<T, uint> powerIt;
 
     public:
-        iterator(T a)
+        iterator(pair<T, uint> it) : powerIt(it) {}
+
+        pair<T, uint> &operator*()
         {
-            one = a;
+            return powerIt;
+        }
+
+        bool operator==(const iterator &other) const
+        {
+            return powerIt.second == other.powerIt.second;
+        }
+        bool operator!=(const iterator &other) const
+        {
+            return powerIt.second != other.powerIt.second;
+        }
+        iterator &operator=(const iterator &other)
+        {
+            powerIt.first = other.powerIt.first;
+            powerIt.second = other.powerIt.second;
+            return *this;
+        }
+        iterator &operator++() //postfix
+        {
+            powerIt.second++;
+            return *this;
+        }
+        const iterator operator++(int) //prefix
+        {
+            iterator temp = *this;
+            powerIt.second++;
+            return temp;
         }
     };
 
 public:
-    powerset() {first=NULL;}
-    powerset(const T a)
+    auto begin() const
     {
-        first = a;
+        return iterator{pair<T, uint>(powerSet, 0)};
     }
-
-    
-    auto begin() { return first.begin(); }
-    auto end() { return first.begin(); }
+    auto end() const
+    {
+        auto powerIter = powerSet.begin();
+        int i = 1;
+        while (powerIter != powerSet.end())
+        {
+            i = i << 1;
+            powerIter++;
+        }
+        return iterator{pair<T, uint>(powerSet, i)};
+    }
 };
+
+template <typename T>
+auto powerset(T power)
+{
+    return powersetIterator<T>(power);
+}
+
 } // namespace itertools
+
+// insert to ostream the correct set by bits
+template <typename T>
+ostream &operator<<(ostream &os, pair<T, uint> &pow)
+{
+    os << "{";
+    auto first = pow.first.begin();
+    auto last = pow.first.end();
+    int i = 1;
+    bool isFirst = true;
+    while (first != last)
+    {
+        if (i & pow.second)
+        {
+            if (!isFirst)
+                os << ",";
+
+            os << *first;
+            isFirst = false;
+        }
+        first++;
+        i = i << 1;
+    }
+    os << "}";
+    return os;
+}
--- product.hpp
+++ product.hpp
@@ -1,40 +1,97 @@
 #pragma once
 #include <iostream>
-#include <iterator>
 using namespace std;
 
 namespace itertools
 {
-template <class T, class U>
-class product
+
+template <typename T, typename U>
+class productIterator
 {
+
 private:
-    T first;
-    U second;
+    const pair<T, U> firstPair; 
+    const pair<T, U> secondPair;
 
 public:
-    product(const T a, const U b)
-    {
-        this->first = a;
-        this->second = b;
-    }
+    productIterator(pair<T, U> &a, pair<T, U> &b) : firstPair(a), secondPair(b) {}
 
     class iterator
     {
     private:
-        T one;
-        U two;
+        pair<T, U> firstPair;
+        pair<U, U> secondPair;
 
     public:
-        iterator(T a, U b)
+        iterator(pair<T, U> a, pair<U, U> b) : firstPair(a), secondPair(b) {}
+
+        pair<T, U> &operator*()
         {
-            one = a;
-            two = b;
+            return firstPair;
         }
 
-        
+        bool operator==(const iterator &ot) const
+        {
+            return firstPair.first == ot.firstPair.first;
+        }
+
+        bool operator!=(const iterator &ot) const
+        {
+            return firstPair.first != ot.firstPair.first;
+        }
+
+        iterator &operator=(const iterator &ot)
+        {
+            this->firstPair.first = ot.firstPair.first;
+            this->firstPair.second = ot.firstPair.second;
+            this->secondPair.first = ot.secondPair.first;
+            this->secondPair.second = ot.secondPair.second;
+            return *this;
+        }
+
+        iterator &operator++()
+        {
+            firstPair.second++;
+            if (firstPair.second == secondPair.second)
+            {
+                firstPair.first++;
+                firstPair.second = secondPair.first;
+            }
+            return *this;
+        }
+
+        const iterator operator++(int)
+        {
+            iterator temp = *this;
+            firstPair.second++;
+            if (firstPair.second == secondPair.second)
+            {
+                firstPair.first++;
+                firstPair.second = secondPair.first;
+            }
+            return temp;
+        }
     };
-    auto begin(){return first.begin();}
-    auto end(){return first.begin();}
+
+public:
+    auto begin()
+    {
+        return iterator{firstPair, pair<U, U>(firstPair.second, secondPair.second)};
+    }
+
+    auto end()
+    {
+        return iterator{pair<T, U>(secondPair.first, secondPair.second), pair<U, U>(firstPair.second, secondPair.second)};
+    }
 };
+
+template <typename T, typename U>
+auto product(T firstProd, U secondProd)
+{
+    pair<decltype(firstProd.begin()), decltype(secondProd.begin())> firstPair(firstProd.begin(), secondProd.begin());
+    pair<decltype(firstProd.end()), decltype(secondProd.end())> secondPair(firstProd.end(), secondProd.end());
+
+    return productIterator<decltype(firstProd.begin()), decltype(secondProd.begin())>(firstPair, secondPair);
+}
+
 } // namespace itertools
