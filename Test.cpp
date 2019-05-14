89151de4b80b41934708ba7f193f25dc8e553601
Doron <doronsds@gmail.com>
2019-05-15 01:49:17 +0300

    tests added.

--- Test.cpp
+++ Test.cpp
@@ -15,24 +15,53 @@ using namespace itertools;
 int main()
 {
 
-    badkan::TestCase testcase;
+    badkan::TestCase tc("Contains");
     int grade = 0;
     int signal = setjmp(badkan::longjmp_buffer);
     if (signal == 0)
     {
-        testcase.setname("containers tests")
-            .CHECK_OK(range(1,8))
+        //////range ///////
+        int a = 1, b = -5, c = 10;
+        string range1 ="", range2 ="", range3 ="";
+        for (int i: range(-4,2)){
+            range1+= to_string(i);}
+        for (int i: range(10,10)){
+            range2+= to_string(i);}
+        for (char i: range('a','d')){
+            range3+= i;}
+        tc.setname("range tests")
+        
+        .CHECK_OUTPUT(range1,"-4-3-2-101")//-4->1
+        .CHECK_OUTPUT(range2,"") //no output since 10=10
+        .CHECK_OUTPUT(range3,"abc");
+       //////~ range//////
+       
+        //////chain///////
+        string chain1="",chain2="",chain3="";
+        for (char i: chain(range('a','c'),range('x','z'))){
+            chain1+= i+" ";}
+        for (int i: chain(range(6,8),range(2,3))){
+            chain2+= to_string(i)+" ";}
+        for (int i: chain(range(6,8),range(1,2))){
+            chain3+= to_string(i)+" ";}   
 
+        tc.setname("chain tests")
+        .CHECK_OUTPUT(chain1,"a b x y ")
+        .CHECK_OUTPUT(chain2,"6 7 2 ")
+        .CHECK_OUTPUT(chain3,"6 7 1 ");
 
-        ;
-        grade = testcase.grade();
+        //////~ chain/////
+       
+
+       
+        grade = tc.grade();
     }
     else
     {
-        testcase.print_signal(signal);
+        tc.print_signal(signal);
         grade = 0;
     }
-    
+
     cout << "Your grade is: " << grade << endl;
     return 0;
 }
